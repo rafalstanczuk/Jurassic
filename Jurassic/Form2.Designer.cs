ead35a32296ef16536797a88b27aa04bff7bc823
Rafał Stańczuk <stanczuk.rafal@gmail.com>
2010-11-03 13:20:18 +0100

    Add Form2 implementation
    - Add Form2 designer file with UI layout
    - Add Form2 resource file with form resources
    - Add Form2 code-behind implementation
    
    Created: 2010-11-03 13:15:24 - 13:20:18

---
+++ Jurassic/Form2.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Jurassic
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonedit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(679, 310);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonedit
+            // 
+            this.buttonedit.Location = new System.Drawing.Point(12, 12);
+            this.buttonedit.Name = "buttonedit";
+            this.buttonedit.Size = new System.Drawing.Size(80, 26);
+            this.buttonedit.TabIndex = 2;
+            this.buttonedit.Text = "Edytuj";
+            this.buttonedit.UseVisualStyleBackColor = true;
+            this.buttonedit.Click += new System.EventHandler(this.buttonedit_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 310);
+            this.Controls.Add(this.buttonedit);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Text = "Wybrany fragment mapy";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonedit;
+        public System.Windows.Forms.PictureBox pictureBox1;
+
+    }
+}
---
+++ Jurassic/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Jurassic
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonedit_Click(object sender, EventArgs e)
+        {
+             Process.Start( pictureBox1.ImageLocation );
+           
+        }
+
+
+
+
+
+
+    }
+}
